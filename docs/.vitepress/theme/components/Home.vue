e7a4bf747f621fa5688e6dc9e23222e981b88861
Antenna <50518073+Anteness@users.noreply.github.com>
2024-01-02 08:54:58 +0800

    变更子标题，优化简介

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -2,14 +2,14 @@
     <HomeBanner />
 
     <div class="links-group links-group-first">
-        <HomeLinkButton text="VRChat 文档枢纽" link="/docs.vrchat.com/"
-            description="您可以找到全面的指南和文档，帮助您尽快开始玩 VRChat，如果遇到困难，还可以获得支持。" icon="/assets/vrchat.svg" />
-        <HomeLinkButton text="VRChat 创作者文档" link="/creators.vrchat.com/" description="利用我们的工具和文档来学习，创作，分享。"
+        <HomeLinkButton text="VRChat 帮助文档" link="/docs.vrchat.com/"
+            description="为您提供游玩入门的指导，让您能够快速上手并充分利用 VRChat 的各项功能。" icon="/assets/vrchat.svg" />
+        <HomeLinkButton text="VRChat 创作者指南" link="/creators.vrchat.com/" description="理解并利用 VRChat 官方工具，学习、创作，实现您的大胆想法！"
             icon="/assets/creators-docs-icon.png" />
     </div>
     <div class="links-group links-group-extra">
         <HomeLinkButton text="UdonSharp" link="/udonsharp.docs.vrchat.com/" description="一个把 C# 代码编译为 UdonAssembly 的编译器。" />
-        <HomeLinkButton text="创作者助手" link="/vcc.docs.vrchat.com/" description="创建 VRChat 内容的新起点。" />
+        <HomeLinkButton text="创作者助手" link="/vcc.docs.vrchat.com/" description="创作 VRChat 内容的新起点。" />
         <HomeLinkButton text="ClientSim" link="/clientsim.docs.vrchat.com/" description="一个允许您在 Unity 内测试 VRChat 世界的工具。" />
     </div>
 </template>
