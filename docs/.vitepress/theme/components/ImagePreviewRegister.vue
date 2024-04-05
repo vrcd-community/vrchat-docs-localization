ae82c37c123b81fa233503449341c82460920b0f
Misaka-L <lipww1234@foxmail.com>
2024-04-05 16:50:44 +0800

    feature: image zoom

---
+++ docs/.vitepress/theme/components/ImagePreviewRegister.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+
+// https://bddxg.top/article/note/vitepress%E4%BC%98%E5%8C%96/%E7%82%B9%E5%87%BB%E5%9B%BE%E7%89%87%E6%94%BE%E5%A4%A7.html
+
+const route = useRoute()
+const initZoom = () => {
+    mediumZoom('.doc-content-image', { background: 'var(--vp-c-bg-soft)' })
+}
+onMounted(() => {
+    initZoom()
+})
+watch(
+    () => route.path,
+    () => nextTick(() => initZoom())
+)
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,29 +1,31 @@
-import { h } from 'vue'
+import { h } from "vue"
 
-import DefaultTheme from 'vitepress/theme'
-import type { Theme } from 'vitepress'
+import DefaultTheme from "vitepress/theme"
+import type { Theme } from "vitepress"
 
-import Home from './components/Home.vue'
-import CreatorsHome from './components/CreatorsHome.vue'
-import RegisterSW from './components/RegisterSW.vue'
-import ToOfficialDocs from './components/ToOfficialDocs.vue'
+import Home from "./components/Home.vue"
+import CreatorsHome from "./components/CreatorsHome.vue"
+import RegisterSW from "./components/RegisterSW.vue"
+import ToOfficialDocs from "./components/ToOfficialDocs.vue"
+import ImagePreviewRegister from "./components/ImagePreviewRegister.vue"
 
-import './css/theme.css'
-import './css/components.css'
+import "./css/theme.css"
+import "./css/components.css"
 import "vue-draggable-resizable/style.css"
 
 const customTheme: Theme = {
-    extends: DefaultTheme,
-    enhanceApp({ app }) {
-        app.component('Home', Home)
-        app.component('CreatorsHome', CreatorsHome)
-    },
-    Layout() {
-        return h(DefaultTheme.Layout, null, {
-            'layout-bottom': () => h(RegisterSW),
-            'aside-outline-after': () => h(ToOfficialDocs)
-        })
-    }
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component("Home", Home)
+    app.component("CreatorsHome", CreatorsHome)
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "layout-bottom": () => h(RegisterSW),
+      "aside-outline-after": () => h(ToOfficialDocs),
+      "doc-bottom": () => h(ImagePreviewRegister),
+    })
+  },
 }
 
-export default customTheme;
+export default customTheme
